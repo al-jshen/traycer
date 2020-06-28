27e24edfd52aa9925b6b02734b5ee2b3c588488c
Jeff Shen <jshen2014@hotmail.com>
2020-06-27 21:21:12 -0400

    hittable list

--- src/hittable.rs
+++ src/hittable.rs
@@ -5,18 +5,29 @@ pub struct HitRecord {
     p: Point3D,
     t: f32,
     normal: Vec3D,
-    front_face: bool,
 }
 
 impl HitRecord {
-    pub fn set_normal_face(&mut self, r: &Ray, outward_normal: &Vec3D) {
-        self.front_face = r.direction().dot(outward_normal) < 0.;
-        self.normal = if self.front_face { *outward_normal } else { -*outward_normal };
+    pub fn new(p: Point3D, t: f32, normal: Vec3D) -> HitRecord {
+        HitRecord {
+            p,
+            t, 
+            normal,
+        }
+    }
+    pub fn p(&self) -> Point3D {
+        self.p
+    }
+    pub fn t(&self) -> f32 {
+        self.t
+    }
+    pub fn normal(&self) -> Vec3D {
+        self.normal
     }
 }
 
 pub trait Hittable {
-    fn hit(&self, r: &Ray, t_min: f32, t_max: f32, rec: &mut HitRecord) -> bool;
+    fn hit(&self, r: &Ray, t_min: f32, t_max: f32) -> Option<HitRecord>;
 }
 
 pub struct Sphere {
@@ -34,7 +45,7 @@ impl Sphere {
 }
 
 impl Hittable for Sphere {
-   fn hit(&self, r: &Ray, t_min: f32, t_max: f32, rec: &mut HitRecord) -> bool {
+   fn hit(&self, r: &Ray, t_min: f32, t_max: f32) -> Option<HitRecord> {
         let sep: Vec3D = r.origin() - self.center;
         let a: f32 = r.direction().length_squared();
         let half_b: f32 = r.direction().dot(&sep);
@@ -43,17 +54,49 @@ impl Hittable for Sphere {
         
         if discriminant > 0. {
             let root: f32 = discriminant.sqrt();
-            let temp = if (t_min..t_max).contains(&((-half_b - root) / a)) {
+            let t = if (t_min..t_max).contains(&((-half_b - root) / a)) {
                 (-half_b - root) / a
             } else {
                 (-half_b + root) / a
             };
-            rec.t = temp;
-            rec.p = r.at(rec.t);
-            let normal = (rec.p - self.center) / self.radius;
-            rec.set_normal_face(r, &normal);
-            return true;
+            let p = r.at(t);
+            let outward_normal = (p - self.center) / self.radius;
+            let normal = if r.direction().dot(&outward_normal) < 0. {
+                outward_normal
+            } else {
+                -outward_normal
+            };
+            Some(HitRecord::new(r.at(t), t, normal));
         }
-        false
+        None
+    }
+}
+
+struct HittableList {
+    objects: Vec<Box<dyn Hittable>>,
+}
+
+impl HittableList {
+    pub fn new(objects: Vec<Box<dyn Hittable>>) -> HittableList {
+        HittableList { objects }
+    }
+    pub fn add(&mut self, object: Box<dyn Hittable>) {
+        self.objects.push(object);
+    }
+    pub fn clear(&mut self) {
+        self.objects.clear();
+    }
+}
+
+impl Hittable for HittableList {
+    fn hit(&self, r: &Ray, t_min: f32, t_max: f32) -> Option<HitRecord> {
+        let mut closest_so_far = t_max;
+        for obj in self.objects.iter() {
+            if let Some(hit_record) = obj.hit(r, t_min, closest_so_far) {
+                closest_so_far = hit_record.t();
+                return Some(HitRecord::new(hit_record.p(), hit_record.t(), hit_record.normal()));
+            }
+        }
+        None
     }
 }
