17b5fb64c6de3a53202e1fa785284ead48cfc3ce
Jeff Shen <jshen2014@hotmail.com>
2020-06-28 15:36:55 -0400

    gamma correction

--- src/colour.rs
+++ src/colour.rs
@@ -1,9 +1,16 @@
 use crate::vec3d::Colour;
 
 pub fn get_colour(pixel_colour: Colour, samples_per_pixel: f32) -> String {
-    let r = (pixel_colour.x() / samples_per_pixel).clamp(0., 0.999) * 256.;
-    let g = (pixel_colour.y() / samples_per_pixel).clamp(0., 0.999) * 256.;
-    let b = (pixel_colour.z() / samples_per_pixel).clamp(0., 0.999) * 256.;
+    let r = transform_colour(pixel_colour.x(), samples_per_pixel);
+    let g = transform_colour(pixel_colour.y(), samples_per_pixel);
+    let b = transform_colour(pixel_colour.z(), samples_per_pixel);
 
-    format!("{} {} {}\n", r as u16, g as u16, b as u16) 
+    format!("{} {} {}\n", r, g, b) 
+}
+
+fn transform_colour(init_val: f32, samples_per_pixel: f32) -> u16 {
+    let mut new_val = init_val / samples_per_pixel;
+    new_val = new_val.sqrt(); // gamma correction
+    new_val = new_val.clamp(0., 0.999) * 256.;
+    new_val as u16
 }
